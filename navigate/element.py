dc4e7040d9f472731330b362e5d562d59fbbdf6e
Piekarski Przemysław <p.piekarski@optopol.com.pl>
2023-11-02 20:02:10 +0100

    navigate/element.py Modify class Element (add region and offset)

--- navigate/element.py
+++ navigate/element.py
@@ -90,33 +90,33 @@ class Element:
                     print(e)
 
         @classmethod
-        def moveTo(cls, image_path, time_limit=None, duration=2, position='center', region=None, offset=0):
+        def moveTo(cls, image_path, time_limit=None, duration=0.5, x=None, y=None, region=None):
             """Przesuwa kursor na środek znalezionego obrazka w określonym czasie"""
+
             if time_limit is None or time_limit <= 0:
                 time_limit = float('inf')
             start_time = time.time()
 
             while True:
                 try:
-                    screenshot = pyautogui.screenshot()
-
                     if region:
                         position_found = pyautogui.locateOnScreen(image_path, region=region, confidence=0.9)
                     else:
                         position_found = pyautogui.locateOnScreen(image_path, confidence=0.9)
+
                     if position_found:
-                        x, y, width, height = position_found
-                        center_x, center_y = x + width / 2, y + height / 2
-                        if position == 'center':
-                            pyautogui.moveTo(center_x, center_y, duration=duration)
-                        elif position == 'left':
-                            pyautogui.moveTo(x + offset, center_y, duration=duration)
-                        elif position == 'right':
-                            pyautogui.moveTo(x + width - offset, center_y, duration=duration)
-                        elif position == 'top':
-                            pyautogui.moveTo(center_x, y + offset, duration=duration)
-                        elif position == 'bottom':
-                            pyautogui.moveTo(center_x, y + height - offset, duration=duration)
+                        x1, y1, width, height = position_found  # Extract coordinates
+
+                        if x is None and y is None:
+                            target_x = x1 + width // 2
+                            target_y = y1 + height // 2
+                        else:
+                            target_x = x1 + x
+                            target_y = y1 + y
+
+                        pyautogui.moveTo(target_x, target_y, duration=duration)
+
+
                         return round(time.time() - start_time, 2)
                     else:
                         if time.time() - start_time > time_limit:
@@ -127,16 +127,24 @@ class Element:
         class Mouse:
             class Left:
                 @classmethod
-                def click(cls, image_path, time_limit=None):
+                def click(cls, image_path, time_limit=None, x=None, y=None, region=None):
                     if time_limit is None or time_limit <= 0:
                         time_limit = float('inf')
                     start_time = time.time()
+
                     while True:
                         try:
-                            screenshot = pyautogui.screenshot()
-                            position = pyautogui.locateOnScreen(image_path, confidence=0.9)
+                            if region:
+                                position = pyautogui.locateOnScreen(image_path, region=region, confidence=0.9)
+                            else:
+                                position = pyautogui.locateOnScreen(image_path, confidence=0.9)
+
                             if position:
-                                pyautogui.click(position, button='left')
+                                x1, y1, width, height = position  # Extract coordinates
+                                target_x = x1 + (x if x is not None else width // 2)
+                                target_y = y1 + (y if y is not None else height // 2)
+
+                                pyautogui.click(target_x, target_y, button='left')
                                 return round(time.time() - start_time, 2)
                             else:
                                 if time.time() - start_time > time_limit:
@@ -145,16 +153,24 @@ class Element:
                             print(e)
 
                 @classmethod
-                def doubleClick(cls, image_path, time_limit=None):
+                def doubleClick(cls, image_path, time_limit=None, x=None, y=None, region=None):
                     if time_limit is None or time_limit <= 0:
                         time_limit = float('inf')
                     start_time = time.time()
+
                     while True:
                         try:
-                            position = pyautogui.locateOnScreen(image_path, confidence=0.9)
-                            print(str(position))
+                            if region:
+                                position = pyautogui.locateOnScreen(image_path, region=region, confidence=0.9)
+                            else:
+                                position = pyautogui.locateOnScreen(image_path, confidence=0.9)
+
                             if position:
-                                pyautogui.doubleClick(position, button='left')
+                                x1, y1, width, height = position  # Extract coordinates
+                                target_x = x1 + (x if x is not None else width // 2)
+                                target_y = y1 + (y if y is not None else height // 2)
+
+                                pyautogui.doubleClick(target_x, target_y, button='left')
                                 return round(time.time() - start_time, 2)
                             else:
                                 if time.time() - start_time > time_limit:
@@ -164,16 +180,24 @@ class Element:
 
             class Right:
                 @classmethod
-                def click(cls, image_path, time_limit=None):
+                def click(cls, image_path, time_limit=None, x=None, y=None, region=None):
                     if time_limit is None or time_limit <= 0:
                         time_limit = float('inf')
                     start_time = time.time()
+
                     while True:
                         try:
-                            screenshot = pyautogui.screenshot()
-                            position = pyautogui.locateOnScreen(image_path, confidence=0.9)
+                            if region:
+                                position = pyautogui.locateOnScreen(image_path, region=region, confidence=0.9)
+                            else:
+                                position = pyautogui.locateOnScreen(image_path, confidence=0.9)
+
                             if position:
-                                pyautogui.click(position, button='right')
+                                x1, y1, width, height = position  # Extract coordinates
+                                target_x = x1 + (x if x is not None else width // 2)
+                                target_y = y1 + (y if y is not None else height // 2)
+
+                                pyautogui.click(target_x, target_y, button='right')
                                 return round(time.time() - start_time, 2)
                             else:
                                 if time.time() - start_time > time_limit:
@@ -182,21 +206,27 @@ class Element:
                             print(e)
 
                 @classmethod
-                def doubleClick(cls, image_path, time_limit=None):
+                def doubleClick(cls, image_path, time_limit=None, x=None, y=None, region=None):
                     if time_limit is None or time_limit <= 0:
                         time_limit = float('inf')
                     start_time = time.time()
+
                     while True:
                         try:
-                            position = pyautogui.locateOnScreen(image_path, confidence=0.9)
-                            print(str(position))
+                            if region:
+                                position = pyautogui.locateOnScreen(image_path, region=region, confidence=0.9)
+                            else:
+                                position = pyautogui.locateOnScreen(image_path, confidence=0.9)
+
                             if position:
-                                pyautogui.doubleClick(position, button='right')
+                                x1, y1, width, height = position  # Extract coordinates
+                                target_x = x1 + (x if x is not None else width // 2)
+                                target_y = y1 + (y if y is not None else height // 2)
+
+                                pyautogui.doubleClick(target_x, target_y, button='right')
                                 return round(time.time() - start_time, 2)
                             else:
                                 if time.time() - start_time > time_limit:
                                     return False
                         except Exception as e:
                             print(e)
-
-
